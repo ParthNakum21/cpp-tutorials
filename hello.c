5d5996bb845ff59ce3bb4d402d1bf9c9c95aeba5
Parth Nakum <parthnakum021@gmail.com>
2024-09-15 13:46:01 +0530

    added sister's name

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@ int main(){
     printf("Hello, World!");
     printf("This is my first github repository");
     printf("My name is Kaushal");
+    printf("My sister's name is divya");
     return 0;
 }
